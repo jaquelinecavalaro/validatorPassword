9c5f2576c49e91faae475fe1f5af61a5bfea876e
Jaqueline C <jaqueline.cavalaro@gmail.com>
2024-04-30 19:05:15 -0300

    feat: upload first commit with spring boot

---
+++ src/main/java/com/example/validatorPassword/ValidatorPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.example.validatorPassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatorPasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/validatorPassword/ValidatorPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validatorPassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatorPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
